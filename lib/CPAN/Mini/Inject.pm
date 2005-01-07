758fdfbc437026cf1bb742b35dc305949aded0cf
ssoriche <ssoriche@91f237bf-2be8-0310-a7b7-ddeaf338d02f>
2005-01-07 00:38:57 +0000

    Updated Changes file from 0.13 to 0.14, then added 0.15. Removed the content from 01mailrc.txt.gz to reduce the overall size of the package. Updated version number in Inject.pm for testing.
    git-svn-id: http://svn.coloredblocks.net/svn/CPAN-Mini-Inject/trunk@37 91f237bf-2be8-0310-a7b7-ddeaf338d02f

--- lib/CPAN/Mini/Inject.pm
+++ lib/CPAN/Mini/Inject.pm
@@ -17,11 +17,11 @@ CPAN::Mini::Inject - Inject modules into a CPAN::Mini mirror.
 
 =head1 Version
 
-Version 0.14
+Version 0.15
 
 =cut
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 our @ISA=qw( CPAN::Mini );
 
 =head1 Synopsis
