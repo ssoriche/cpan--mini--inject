da4d05bfb95f69339f4a40baaec0bd8d89b9d87d
ssoriche <ssoriche@91f237bf-2be8-0310-a7b7-ddeaf338d02f>
2005-01-09 03:24:28 +0000

    Preparing for release of version 0.16
    git-svn-id: http://svn.coloredblocks.net/svn/CPAN-Mini-Inject/trunk@40 91f237bf-2be8-0310-a7b7-ddeaf338d02f

--- lib/CPAN/Mini/Inject.pm
+++ lib/CPAN/Mini/Inject.pm
@@ -17,11 +17,11 @@ CPAN::Mini::Inject - Inject modules into a CPAN::Mini mirror.
 
 =head1 Version
 
-Version 0.15
+Version 0.16
 
 =cut
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 our @ISA=qw( CPAN::Mini );
 
 =head1 Synopsis
